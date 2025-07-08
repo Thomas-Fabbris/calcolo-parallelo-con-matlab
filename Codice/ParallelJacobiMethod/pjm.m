b671f95ee2389a97a1f65208c2fe83c1db875e2a
Thomas Fabbris <115624667+Thomas-Fabbris@users.noreply.github.com>
2025-07-09 00:20:37 +0200

    Migliorato metodo di Jacobi

--- Codice/ParallelJacobiMethod/pjm.m
+++ Codice/ParallelJacobiMethod/pjm.m
@@ -14,88 +14,79 @@ function [x,flag, relres, iter, resvec] = pjm(A,b,tol,maxit)
 %
 %Input Arguments
 %   A - Coefficient matrix
-%     matrix
+%      matrix
 %   b - Right side of linear equation
-%     column vector
+%      column vector
 %   tol - Method tolerance
-%     [] or 1e-6 (default) | positive scalar
+%      [] or 1e-6 (default) | positive scalar
 %   maxit - Maximum number of iterations
-%     [] or min(size(A,1),100) (default) | positive scalar integer
+%      [] or 100 (default) | positive scalar integer
 %
 %Output Arguments
 %   x - Linear system solution
-%     column vector
+%      column vector
 %   flag - Convergence flag
-%     scalar
+%      scalar
 %   relres - Relative residual error
-%     scalar
+%      scalar
 %   iter - Iteration number
-%     scalar
+%      scalar
 %   resvec - Residual error
-%     vector
+%      vector
 %
 %See also parpool, gcp, distributed, gather
 
 arguments (Input)
     A {mustBeMatrix, mustBeFloat, mustBeSquared}
-    b {mustBeColumn, mustBeFloat}
+    b {mustBeColumn, mustBeFloat, mustBeSameSize(A,b)}
     tol (1,1) {mustBeFloat, mustBePositive} = PjmHelper.DefaultTol
     maxit (1,1) {mustBeInteger, mustBePositive} = PjmHelper.DefaultMaxit
 end
 
 arguments (Output)
     x (:,1) {mustBeFloat}
-    flag (1,1) {mustBeMember(flag, [0, 1])}
-    relres (1,1) {mustBeFloat, mustBeBetween(relres, 0, 1)}
-    iter (1,1) {mustBeInteger, mustBePositive}
+    flag (1,1) {mustBeMember(flag, [0,1,2])}
+    relres (1,1) {mustBeFloat}
+    iter (1,1) {mustBeInteger, mustBeNonnegative}
     resvec (:,1) {mustBeFloat, mustBeNonnegative}
 end
 
 nargoutchk(PjmHelper.MinOutArgs,PjmHelper.MaxOutArgs);
 
-n = size(A,1);
-if n ~= length(b)
-    errID = 'pjm:DimensionMismatch';
-    msg = ['Right-hand side must be a column vector of length 1000 to match the coefficient ' ...
-        'matrix'];
-    throw(MException(errID,msg));
-end
-
-maxit = min(n, maxit);
-
-if isempty(gcp('nocreate'))
-    parpool;
-end
+isInputDistributed = isa(A, 'distributed') || isa(b, 'distributed');
 
 x_dist = distributed(zeros(n,1));
+
 b_dist = distributed(b);
+
 norm_b_dist = norm(b_dist);
 
-if norm_b_dist == 0
-    x = gather(x_dist); flag = 0; relres = 0; iter = 0; resvec = 0;
+if (norm_b_dist == 0)
+
+    if(~isInputDistributed)
+        x = gather(x_dist);
+    end
+
+    flag = 0; relres = 0; iter = 0; resvec = 0;
+
     if(nargout < 2)
         fprintf(['The right hand side vector is all zero so pjm returned an all zero solution ' ...
-            '            without iterating.']);
+            ' without iterating.']);
     end
     return;
 end
 
 A_dist = distributed(A);
-P_dist = distributed(diag(A));
 
-if(any(P_dist == 0))
-    warning('pjm:ZeroDiagonal', 'The coefficient matrix has at least one zero on its main diagonal. ' + ...
-        'This can lead to unexpected output.')
-end
+P_dist = diag(A_dist);
 
 r_dist = b_dist;
 norm_r_dist = norm_b_dist;
-
 relres = 1;
 resvec = zeros(maxit+1, 1);
 resvec(1) = norm_r_dist;
 k = 0;
-flag = 1;
+flag = 1; %Assume failure until convergence
 
 while (relres > tol && k < maxit)
 
@@ -104,30 +95,47 @@ while (relres > tol && k < maxit)
     x_dist = x_dist + z_dist;
     r_dist = r_dist - A_dist * z_dist;
 
-    norm_r = norm(r_dist);
-    resvec(k+1) = norm_r;
+    norm_r_dist = norm(r_dist);
 
-    relres = norm_r / norm_b_dist;
+    if(isnan(norm_r_dist) || isinf (norm_r_dist))
+        flag = 2;
+        break;
+    end
+
+    resvec(k+1) = norm_r_dist;
+    relres = norm_r_dist / norm_b_dist;
+
 end
 
-x = gather(x_dist);
-resvec = resvec(1:k+1);
+if ~isInputDistributed
+    x = gather(x_dist);
+else
+    x = x_dist;
+end
+
+relres = gather(relres);
+resvec = resvec(1:k);
 iter = k;
 
 if(relres<=tol)
     flag = 0;
-    if(nargout < 2)
-        fprintf("pjm converged at iteration %u to a solution with relative residual %.4e.", k, ...
-            relres);
-    end
-else
-    if(nargout < 2)
-        fprintf("pjm stopped at iteration %u without converging to the desired tolerance %.4e " + ...
-            "because the maximum number of iterations %u was reached.\n The iterate returned " + ...
-            "(number %u) has relative residual %.4e.", k,tol,maxit,relres);
-    end
 end
 
+if(nargout < 2)
+    switch flag
+        case 0
+            fprintf("pjm converged at iteration %u to a solution with relative residual %.4e." ...
+                , k,relres);
+        case 1
+            fprintf("pjm stopped at iteration %u without converging to the desired tolerance " + ...
+                "%.4e because the maximum number of iterations %u was reached.\n The iterate " + ...
+                "returned (number %u) has relative residual %.4e.", k,tol,maxit,relres);
+        case 2
+            fprintf("pjm stopped at iteration %u without converging to the desired tolerance %.4e " + ...
+                "a scalar quantity became too small or too large to continue computing.\n " + ...
+                "The iterate returned (number %u) has relative residual %.4e.", k,tol,relres);
+    end
+end
 end
 
 function mustBeSquared(A)
@@ -138,3 +146,12 @@ if(diff(size(A)))
 end
 end
 
+function mustBeSameSize(A,b)
+    n = size(A,1);
+    if (n ~= length(b))
+        errID = 'pjm:DimensionMismatch';
+        msg = sprintf(['Right-hand side must be a column vector of length %u to match the ' ...
+        'coefficient matrix'], n);
+        throw(MException(errID,msg));
+    end
+end
