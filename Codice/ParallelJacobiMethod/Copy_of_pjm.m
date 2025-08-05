f69dc559a676c767fb2ed38a6efc60539bac405f
Thomas Fabbris <115624667+Thomas-Fabbris@users.noreply.github.com>
2025-08-05 18:56:47 +0200

    Update PJM

---
+++ Codice/ParallelJacobiMethod/Copy_of_pjm.m
@@ -0,0 +1,161 @@
+function [x,flag, relres, iter, resvec] = pjm(A,b,tol,maxit,x0)
+%PJM - Solve system of linear equations - Parallel Jacobi Method
+%   This MATLAB function attempts to solve the system of linear equations
+%   A*x = b for x using a parallel implementation of the Jacobi Method.
+%
+%Syntax
+%   x = PJM(A,b)
+%   x = PJM(A,b,tol)
+%   x = PJM(A,b,tol,maxit)
+%   x = PJM(A,b,tol,maxit,x0)
+%   [x,flag] = PJM(___)
+%   [x,flag,relres] = PJM(___)
+%   [x,flag,relres,iter] = PJM(___)
+%   [x,flag,relres,iter,resvec] = PJM(___)
+%
+%Input Arguments
+%   A - Coefficient matrix
+%      matrix
+%   b - Right side of linear equation
+%      column vector
+%   tol - Method tolerance
+%      [] or 1e-6 (default) | positive scalar
+%   maxit - Maximum number of iterations
+%      [] or 100 (default) | positive scalar integer
+%   x0 - Initial Guess
+%      [] or column vector of zeros (default) | column vector
+%
+%Output Arguments
+%   x - Linear system solution
+%      column vector
+%   flag - Convergence flag
+%      scalar
+%   relres - Relative residual error
+%      scalar
+%   iter - Iteration number
+%      scalar
+%   resvec - Residual error
+%      vector
+%
+%See also parpool, gcp, distributed, gather
+
+arguments (Input)
+    A {mustBeMatrix, mustBeFloat, mustBeSquared}
+    b {mustBeColumn, mustBeFloat, mustBeSameSize(A,b, 'Right-hand side')}
+    tol (1,1) {mustBeFloat, mustBePositive} = PjmHelper.DefaultTol
+    maxit (1,1) {mustBeInteger, mustBePositive} = PjmHelper.DefaultMaxit
+    x0 {mustBeColumn, mustBeFloat, mustBeSameSize(A, x0, 'Initial guess')} = zeros(size(A,1))
+end
+
+arguments (Output)
+    x (:,1) {mustBeFloat}
+    flag (1,1) {mustBeMember(flag, [0,1,2])}
+    relres (1,1) {mustBeFloat}
+    iter (1,1) {mustBeInteger, mustBeNonnegative}
+    resvec (:,1) {mustBeFloat, mustBeNonnegative}
+end
+
+nargoutchk(PjmHelper.MinOutArgs,PjmHelper.MaxOutArgs);
+
+isInputDistributed = isa(A, 'distributed') || isa(b, 'distributed');
+n = size(A,1);
+
+x_dist = distributed(zeros(n,1));
+b_dist = distributed(b);
+x0_dist = distributed(x0);
+
+norm_b_dist = norm(b_dist);
+
+if (norm_b_dist == 0)
+    
+    if(~isInputDistributed)
+        x = gather(x_dist);
+    end
+    
+    flag = 0; relres = 0; iter = 0; resvec = 0;
+    
+    if(nargout < 2)
+        fprintf(['The right-hand side vector is all zero so pjm returned an all zero solution ' ...
+            ' without iterating.']);
+    end
+    
+    return;
+end
+
+A_dist = distributed(A);
+P_dist = diag(A_dist);
+
+r_dist = b_dist - x0_dist;
+norm_r_dist = norm_b_dist;
+relres = 1;
+resvec = zeros(maxit+1, 1);
+resvec(1) = norm_r_dist;
+k = 0;
+flag = 1;
+
+while (relres > tol && k < maxit)
+    
+    k = k+1;
+    z_dist = r_dist ./ P_dist;
+    x_dist = x_dist + z_dist;
+    r_dist = r_dist - A_dist * z_dist;
+    
+    norm_r_dist = norm(r_dist);
+    
+    if(isnan(norm_r_dist) || isinf (norm_r_dist))
+        flag = 2;
+        break;
+    end
+    
+    resvec(k+1) = norm_r_dist;
+    relres = norm_r_dist / norm_b_dist;
+    
+end
+
+if ~isInputDistributed
+    x = gather(x_dist);
+else
+    x = x_dist;
+end
+
+relres = gather(relres);
+resvec = resvec(1:k);
+iter = k;
+
+if(relres<=tol)
+    flag = 0;
+end
+
+if(nargout < 2)
+    switch flag
+        case 0
+            fprintf("pjm converged at iteration %u to a solution with relative residual %.4e." ...
+                , k,relres);
+        case 1
+            fprintf("pjm stopped at iteration %u without converging to the desired tolerance " + ...
+                "%.4e because the maximum number of iterations %u was reached.\n The iterate " + ...
+                "returned (number %u) has relative residual %.4e.", k,tol,maxit,relres);
+        case 2
+            fprintf("pjm stopped at iteration %u without converging to the desired tolerance %.4e " + ...
+                "a scalar quantity became too small or too large to continue computing.\n " + ...
+                "The iterate returned (number %u) has relative residual %.4e.", k,tol,relres);
+    end
+end
+end
+
+function mustBeSquared(A)
+if(diff(size(A)))
+    errID = 'pjm:NotSquared';
+    msg = 'Coefficient matrix must be squared.';
+    throw(MException(errID,msg));
+end
+end
+
+function mustBeSameSize(A,v,name)
+if (~isequal(size(A,1), size(v)))
+    errID = 'pjm:DimensionMismatch';
+    msg = sprintf(['%s must be a column vector of length %u to match the ' ...
+        'problem size'],name,n);
+    throw(MException(errID,msg));
+end
+end
--- Codice/ParallelJacobiMethod/pjm.m
+++ Codice/ParallelJacobiMethod/pjm.m
@@ -1,75 +1,74 @@
-function [x,flag, relres, iter, resvec] = pjm(A,b,tol,maxit)
 %PJM - Solve system of linear equations - Parallel Jacobi Method
-%   This MATLAB function attempts to solve the system of linear equations
-%   A*x = b for x using a parallel implementation of the Jacobi Method.
+%    This MATLAB function attempts to solve the system of linear equations
+%    A*x = b for x using a parallel implementation of the Jacobi Method.
 %
 %Syntax
-%   x = PJM(A,b)
-%   x = PJM(A,b,tol)
-%   x = PJM(A,b,tol,maxit)
-%   [x,flag] = PJM(___)
-%   [x,flag,relres] = PJM(___)
-%   [x,flag,relres,iter] = PJM(___)
-%   [x,flag,relres,iter,resvec] = PJM(___)
+%    x = PJM(A,b)
+%    x = PJM(A,b,tol)
+%    x = PJM(A,b,tol,maxit)
+%    x = PJM(A,b,tol,maxit,x0)
+%    [x,flag] = PJM(___)
+%    [x,flag,relres] = PJM(___)
+%    [x,flag,relres,iter] = PJM(___)
+%    [x,flag,relres,iter,resvec] = PJM(___)
 %
 %Input Arguments
-%   A - Coefficient matrix
-%      matrix
-%   b - Right side of linear equation
-%      column vector
-%   tol - Method tolerance
-%      [] or 1e-6 (default) | positive scalar
-%   maxit - Maximum number of iterations
-%      [] or 100 (default) | positive scalar integer
+%    A - Coefficient matrix
+%        matrix
+%    b - Right side of linear equation
+%        column vector
+%    tol - Method tolerance
+%        [] or 1e-6 (default) | positive scalar
+%    maxit - Maximum number of iterations
+%        [] or 100 (default) | positive scalar integer
+%    x0 - Initial Guess
+%        [] or column vector of zeros (default) | column vector
 %
 %Output Arguments
-%   x - Linear system solution
-%      column vector
-%   flag - Convergence flag
-%      scalar
-%   relres - Relative residual error
-%      scalar
-%   iter - Iteration number
-%      scalar
-%   resvec - Residual error
-%      vector
+%    x - Linear system solution
+%        column vector
+%    flag - Convergence flag
+%        scalar
+%    relres - Relative residual error
+%        scalar
+%    iter - Iteration number
+%        scalar
+%    resvec - Residual error
+%        vector
 %
-%See also parpool, gcp, distributed, gather
+%See also parpool, distributed, gather
 
+function [x,flag, relres, iter, resvec] = pjm(A,b,tol,maxit,x0)
 arguments (Input)
-    A {mustBeMatrix, mustBeFloat, mustBeSquared}
-    b {mustBeColumn, mustBeFloat, mustBeSameSize(A,b)}
-    tol (1,1) {mustBeFloat, mustBePositive} = PjmHelper.DefaultTol
-    maxit (1,1) {mustBeInteger, mustBePositive} = PjmHelper.DefaultMaxit
+    A {mustBeMatrix, mustBeFloat, mustBeReal, mustBeSquared}
+    b {mustBeColumn, mustBeFloat, mustBeReal, mustBeSameSize(b,A,'Right-hand side')}
+    tol {mustBeScalarOrEmpty, mustBeFloat, mustBePositive} = 1e-6
+    maxit {mustBeScalarOrEmpty, mustBeInteger, mustBePositive} = 1000
+    x0 {mustBeColumn, mustBeFloat, mustBeSameSize(x0,A,'Initial guess')} = zeros(size(A,1),1)
 end
 
 arguments (Output)
-    x (:,1) {mustBeFloat}
+    x (:,1) {mustBeColumn, mustBeFloat}
     flag (1,1) {mustBeMember(flag, [0,1,2])}
-    relres (1,1) {mustBeFloat}
+    relres (1,1) {mustBeFloat, mustBeBetween(relres,0,1)}
     iter (1,1) {mustBeInteger, mustBeNonnegative}
-    resvec (:,1) {mustBeFloat, mustBeNonnegative}
+    resvec (:,1) {mustBeColumn, mustBeFloat, mustBeNonnegative}
 end
 
-nargoutchk(PjmHelper.MinOutArgs,PjmHelper.MaxOutArgs);
+nargoutchk(0,5);
+isInputDistributed = isa(A, 'distributed') || isa(b, 'distributed') || isa(x0, 'distributed');
 
-isInputDistributed = isa(A, 'distributed') || isa(b, 'distributed');
-n = size(A,1);
+normB = norm(b);
 
-x_dist = distributed(zeros(n,1));
-b_dist = distributed(b);
-norm_b_dist = norm(b_dist);
-
-if (norm_b_dist == 0)
-    
-    if(~isInputDistributed)
-        x = gather(x_dist);
-    end
-    
-    flag = 0; relres = 0; iter = 0; resvec = 0;
+if (normB == 0)
+    x = x0; 
+    flag = 0; 
+    relres = 0; 
+    iter = 0; 
+    resvec = zeros(1,1);
     
     if(nargout < 2)
-        fprintf(['The right hand side vector is all zero so pjm returned an all zero solution ' ...
+        fprintf(['The right-hand side vector is all zero so pjm returned the initial solution ' ...
             ' without iterating.']);
     end
     
@@ -77,33 +76,32 @@ if (norm_b_dist == 0)
 end
 
 A_dist = distributed(A);
-P_dist = diag(A_dist);
+b_dist = distributed(b);
+x_dist = distributed(x0);
+d_dist = diag(A_dist);
+r_dist = b_dist - A_dist*x_dist;
 
-r_dist = b_dist;
-norm_r_dist = norm_b_dist;
-relres = 1;
+norm_r_dist = norm(r_dist);
+relres = norm_r_dist / normB;
 resvec = zeros(maxit+1, 1);
 resvec(1) = norm_r_dist;
-k = 0;
+iter = 0;
 flag = 1;
 
-while (relres > tol && k < maxit)
-    
-    k = k+1;
-    z_dist = r_dist ./ P_dist;
-    x_dist = x_dist + z_dist;
-    r_dist = r_dist - A_dist * z_dist;
-    
+while (relres > tol && iter < maxit)
+    iter = iter+1;
+
+    x_dist = x_dist + r_dist ./d_dist;
+    r_dist = b_dist - A_dist * x_dist;
     norm_r_dist = norm(r_dist);
-    
+
     if(isnan(norm_r_dist) || isinf (norm_r_dist))
         flag = 2;
         break;
     end
     
-    resvec(k+1) = norm_r_dist;
-    relres = norm_r_dist / norm_b_dist;
-    
+    resvec(iter+1) = norm_r_dist;
+    relres = norm_r_dist / normB;
 end
 
 if ~isInputDistributed
@@ -112,9 +110,7 @@ else
     x = x_dist;
 end
 
-relres = gather(relres);
-resvec = resvec(1:k);
-iter = k;
+resvec = resvec(1:iter + 1);
 
 if(relres<=tol)
     flag = 0;
@@ -124,15 +120,15 @@ if(nargout < 2)
     switch flag
         case 0
             fprintf("pjm converged at iteration %u to a solution with relative residual %.4e." ...
-                , k,relres);
+                , iter,relres);
         case 1
             fprintf("pjm stopped at iteration %u without converging to the desired tolerance " + ...
                 "%.4e because the maximum number of iterations %u was reached.\n The iterate " + ...
-                "returned (number %u) has relative residual %.4e.", k,tol,maxit,relres);
+                "returned (number %u) has relative residual %.4e.", iter,tol,maxit,relres);
         case 2
-            fprintf("pjm stopped at iteration %u without converging to the desired tolerance %.4e " + ...
-                "a scalar quantity became too small or too large to continue computing.\n " + ...
-                "The iterate returned (number %u) has relative residual %.4e.", k,tol,relres);
+            fprintf("pjm stopped at iteration %u without converging to the desired tolerance " + ...
+                "%.4e a scalar quantity became too small or too large to continue computing.\n " + ...
+                "The iterate returned (number %u) has relative residual %.4e.", iter,tol,relres);
     end
 end
 end
@@ -145,12 +141,12 @@ if(diff(size(A)))
 end
 end
 
-function mustBeSameSize(A,b)
+function mustBeSameSize(v,A,name)
 n = size(A,1);
-if (n ~= length(b))
+if (~isequal(n, size(v,1)))
     errID = 'pjm:DimensionMismatch';
-    msg = sprintf(['Right-hand side must be a column vector of length %u to match the ' ...
-        'coefficient matrix'], n);
+    msg = sprintf(['%s must be a column vector of length %u to match the ' ...
+        'problem size'],name,n);
     throw(MException(errID,msg));
 end
 end
